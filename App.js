79626b3e9e255a777ff062119f7957801d1a6beb
Mobile App Dev <mad@Mobile-App-Development-1.local>
2020-11-02 17:34:56 -0500

    made topBar  a component and tried to call it

--- App.js
+++ App.js
@@ -1,18 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, VirtualizedList, Image, TextInput  } from 'react-native';
-
-const WordInput = () => {
-    const [value, onChangeText] = React.useState('Placeholder text');
- 
-    return (
-        <TextInput
-        style={{ height: 46, borderColor: 'black', borderWidth: 1, backgroundColor:'gray' }}
-         onChangeText={text => onChangeText(text)}
-      value={value}
-    />
-    )
-}
+import topBar from './components/topBar.js'
 
 export default function App() {
   return (
@@ -21,7 +10,7 @@ export default function App() {
       padding: 50,
       backgroundColor:"#B529F8",
     }}>
-       <WordInput/>
+       {topBar}
       <StatusBar/>
     </View>
   );
--- components/topBar.js
+++ components/topBar.js
@@ -1,3 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, VirtualizedList, Image, TextInput  } from 'react-native';
+
+export function topBar () {
+const WordInput = () => {
+    const [value, onChangeText] = React.useState('Placeholder text');
+ 
+    return (
+        <TextInput
+        style={{ height: 46, borderColor: 'black', borderWidth: 1, backgroundColor:'gray' }}
+         onChangeText={text => onChangeText(text)}
+      value={value}
+    />
+    )
+}
+};
