c8d727f32c98d3c2a5cece6a42c7b7bb493fd987
Mobile App Dev <mad@Mobile-App-Development-1.local>
2020-11-03 12:18:28 -0500

    Went back to older design

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, VirtualizedList, Image, TextInput  } from 'react-native';
-import topBar from './components/topBar.js'
+
+const WordInput = () => {
+  const [value, onChangeText] = React.useState('Placeholder text');
+
+return (
+  <TextInput style={{ 
+  height: 46, borderColor: 'black', borderWidth: 1, backgroundColor:'#FEFEFE', opacity: 75, border
+}} 
+  onChangeText={text => onChangeText(text)} 
+  value={value}/>
+)
+};
 
 export default function App() {
+  
   return (
-    {topBar},
     <View style={{
       height: 80,
       padding: 50,
       backgroundColor:"#B529F8",
     }}>
+      <WordInput />
       <StatusBar/>
     </View>
   );
-};
+}
+
--- components/topBar.js
+++ components/topBar.js
@@ -2,16 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, VirtualizedList, Image, TextInput  } from 'react-native';
 
-export default function topBar () {
+export function as (topBar) {
 const WordInput = () => {
     const [value, onChangeText] = React.useState('Placeholder text');
  
     return (
+        <View style={{
+            height: 80,
+            padding: 50,
+            backgroundColor:"#B529F8",
+          }}>   
         <TextInput
         style={{ height: 46, borderColor: 'black', borderWidth: 1, backgroundColor:'gray' }}
          onChangeText={text => onChangeText(text)}
       value={value}
     />
+    </View>
     )
 }
 };
