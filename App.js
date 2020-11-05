e7e614b9d200251d1b9273d7d21ea7b7480ac527
Garrett Michael Roberts <mad@dbcs-f.k12k.com>
2020-11-05 11:48:52 -0500

    Imported word input

--- App.js
+++ App.js
@@ -1,32 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, Image, TextInput, SafeAreaView, TouchableOpacity  } from 'react-native';
-
-const WordInput = () => {
-  const [value, onChangeText] = React.useState('Placeholder text');
-
-return (
-  <TextInput style={{ 
-  height: 46, borderColor: 'black', borderWidth: 1, backgroundColor:'#FEFEFE', opacity: 75, 
-}} 
-  onChangeText={text => onChangeText(text)} 
-  value={value}/>
-)
-};
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Image,
+  TextInput,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import WordInput from "./components/topBar";
 
 export default function App() {
-  
   return (
-    <View style={{
-      height: 80,
-      padding: 70,
-      backgroundColor:"#B529F8",
-    }}>
+    <View
+      style={{
+        height: 80,
+        padding: 70,
+        backgroundColor: "#B529F8",
+      }}
+    >
       <WordInput />
-      <StatusBar/>
+      <StatusBar />
     </View>
-//    <View>
-//      <Page />
-//    </View>
   );
 }
--- components/topBar.js
+++ components/topBar.js
@@ -1,23 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, VirtualizedList, Image, TextInput  } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
-export function as (topBar) {
-const WordInput = () => {
-    const [value, onChangeText] = React.useState('Placeholder text');
- 
-    return (
-        <View style={{
-            height: 80,
-            padding: 50,
-            backgroundColor:"#B529F8",
-          }}>   
-        <TextInput
-        style={{ height: 46, borderColor: 'black', borderWidth: 1, backgroundColor:'gray' }}
-         onChangeText={text => onChangeText(text)}
+export default WordInput = () => {
+  const [value, onChangeText] = React.useState("Placeholder text");
+
+  return (
+    <TextInput
+      style={{
+        height: 46,
+        borderColor: "black",
+        borderWidth: 1,
+        backgroundColor: "#FEFEFE",
+        opacity: 75,
+      }}
+      onChangeText={(text) => onChangeText(text)}
       value={value}
     />
-    </View>
-    )
-}
+  );
 };
