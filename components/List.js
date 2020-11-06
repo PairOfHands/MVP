4ac8dbe80e734c0066b54f34822372e26ccc55be
Garrett Michael Roberts <mad@dbcs-f.k12k.com>
2020-11-06 11:26:49 -0500

    more list items

--- components/List.js
+++ components/List.js
@@ -10,16 +10,19 @@ import {
 
 const DATA = [
   {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
     title: "Bailey Buddy",
   },
   {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
     title: "Decent App",
   },
   {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
     title: "Meh App",
+  },
+  {
+    title: "Bad App",
+  },
+  {
+    title: "Unrated App",
   },
 ];
 
@@ -51,12 +54,15 @@ export default AppList = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column",
     marginTop: StatusBar.currentHeight || 0,
   },
   item: {
+    width: 150,
     padding: 20,
-    marginVertical: 8,
+    marginVertical: 16,
     marginHorizontal: 16,
+    backgroundColor: "#CBC3C0",
   },
   title: {
     fontSize: 32,
