b51a33446c197349c9e47a16df1db877d9caef8f
Garrett Michael Roberts <mad@dbcs-f.k12k.com>
2020-11-24 11:29:49 -0500

    imported app rate

--- screens/RatingScreen.js
+++ screens/RatingScreen.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet } from "react-native";
 import FirstApp from "../components/nameBar";
 import AppRev from "../components/appDesc";
 import AppPic from "../components/appLogo";
+import AppRate from "../components/appRating";
 import SeeRev from "../components/seeReview";
 
 const Data = [{ id: 1 }];
@@ -17,6 +18,7 @@ function RatingScreen({ navigation, route }) {
       <FirstApp />
       <AppPic />
       <AppRev />
+      <AppRate />
       <SeeRev navigation={navigation} />
     </View>
   );
