b01c0fddc7336cb5c387c30c7b4e83ef90de010d
Garrett Michael Roberts <mad@dbcs-f.k12k.com>
2020-11-24 11:13:18 -0500

    added Ameno

--- components/appDesc.js
+++ components/appDesc.js
@@ -19,7 +19,20 @@ export default AppRev = () => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollView}>
-        <Text style={styles.text}>This description can scroll</Text>
+        <Text style={styles.text}>
+          Dori me, interimo Ayapare, dorime Ameno, ameno Latire, latiremo Dori
+          me Ameno Omenare imperavi ameno Dimere, dimere matiro Matiremo, ameno
+          Omenare imperavi emunari Ameno Omenare imperavi emunari Ameno Ameno
+          dore Ameno dori me Ameno dori me Ameno dom Dori me reo Ameno dori me
+          Ameno dori me Dori me am Ameno Ameno Ameno Omenare imperavi ameno
+          Dimere dimere martiro Martiremo, ameno Omenare imperavi emunari Ameno
+          Omenare imperavi emunari Ameno Ameno dore Ameno dori me Ameno dori me
+          Ameno dom Dori me reo Ameno dori me Ameno dori me Dori me am Ameno,
+          ameno Ameno dori me Ameno dori me Dori me Ameno Ameno dore Ameno dori
+          me Ameno dori me Ameno dom Dori me reo Ameno dori me Ameno dori me
+          Ameno, ameno Ameno Ameno dori me Ameno Ameno dori me Ameno dom Dori me
+          reo, ameno Ameno dori me Ameno, ameno dori me
+        </Text>
       </ScrollView>
     </SafeAreaView>
   );
