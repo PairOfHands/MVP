085696c01e3d6d2a894f58c3be45f039eb3af265
Garrett Michael Roberts <mad@dbcs-f.k12k.com>
2020-12-10 12:10:35 -0500

    added buttons

--- components/rateReview.js
+++ components/rateReview.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Button,
+  Alert,
+} from "react-native";
 
 const styles = StyleSheet.create({
   container: {
@@ -12,13 +19,14 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     borderWidth: 0.5,
   },
-  title: {},
 });
 
 export default RateRev = () => {
   return (
     <View style={styles.container}>
       <Text>was this review helpful</Text>
+      <Button title="Yes" />
+      <Button title="No" onPress={() => Alert.alert("Simple Button pressed")} />
     </View>
   );
 };
--- components/revName.js
+++ components/revName.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
